43e18d905413e6a01a574e44812b8248c1127549
Jaisingh123113 <yadavjaysingh420@gmail.com>
2025-03-19 15:08:34 +0530

    add task

---
+++ src/main/java/com/backend/project_management/Entity/Task.java
@@ -0,0 +1,36 @@
+package com.backend.project_management.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String description;
+    private String projectName;
+    private int days;
+    private int hour;
+    private String status;
+    @Column(name = "statusBar", nullable = false)
+    private double statusBar =0;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String imageUrl;
+    private long durationInMinutes;
+    private String subject;
+
+}
