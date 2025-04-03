0ceca894305fc0bc09124f937f0c3cf085a49172
tambolisimran <simrantamboli211@gmail.com>
2025-04-03 10:35:54 +0530

    add prop

--- src/main/java/com/backend/project_management/Controller/ProjectController.java
+++ src/main/java/com/backend/project_management/Controller/ProjectController.java
@@ -10,10 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/Project")
-@CrossOrigin(origins = "http://localhost:3000")
+
 
 public class ProjectController {
     @Autowired
