3a6394637332a6cb09a765f7e4c220438393e13d
SHUBHAMMUNGSE <154522853+SHUBHAMMUNGSE@users.noreply.github.com>
2025-05-16 15:15:13 +0530

    new api for update image Url in team member

--- src/main/java/com/backend/project_management/Configuration/SecurityConfig.java
+++ src/main/java/com/backend/project_management/Configuration/SecurityConfig.java
@@ -105,6 +105,7 @@ public class SecurityConfig {
                         .requestMatchers(PUT,"/teams/**").hasAuthority(ADMIN_UPDATE.name())
                         .requestMatchers(DELETE,"/teams/**").hasAuthority(ADMIN_DELETE.name())
                         .requestMatchers("/forgetPassword/**").permitAll()
+                        .requestMatchers("/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
