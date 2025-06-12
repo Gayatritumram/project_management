5441beda74a20584d61de2c1d00310b40fe654a2
Jaisingh123113 <yadavjaysingh420@gmail.com>
2025-06-12 16:37:38 +0530

    added depetment antity

---
+++ src/main/java/com/backend/project_management/Entity/Department.java
@@ -0,0 +1,30 @@
+package com.backend.project_management.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "PM_Department")
+public class Department {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String departmentName;
+
+        private String createdByEmail;
+
+        private String role;
+
+        private String branchCode;
+
+        private LocalDateTime createdDate;
+
+    }
