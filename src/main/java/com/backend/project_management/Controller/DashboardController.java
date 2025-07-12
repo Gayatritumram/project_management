d0c18867701a81dda80c7a5a4e9f1e5bc3cd72f0
GayatriT17 <tumramgayatri27@gmail.com>
2025-07-12 12:31:01 +0530

    Added GetAllProjects in Dashboard

--- src/main/java/com/backend/project_management/Controller/DashboardController.java
+++ src/main/java/com/backend/project_management/Controller/DashboardController.java
@@ -1,11 +1,19 @@
 package com.backend.project_management.Controller;
 
+import com.backend.project_management.DTO.ProjectDTO;
 import com.backend.project_management.DTO.TaskCountDTO;
 import com.backend.project_management.DTO.TaskDashboardDTO;
+import com.backend.project_management.DTO.TeamLeaderDTO;
+import com.backend.project_management.Service.ProjectService;
 import com.backend.project_management.Service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +22,8 @@ public class DashboardController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private ProjectService projectService;
     // dashboard by status
     @GetMapping("/dashboard/byStatus")
     public ResponseEntity<TaskDashboardDTO> getDashboard(
@@ -32,9 +42,25 @@ public class DashboardController {
     ) {
         return ResponseEntity.ok(taskService.getTaskCountsByTimeFrame(branchCode, role, email));
     }
+    @GetMapping("/getAllProjects")
+    public ResponseEntity<List<Map<String, Object>>> getAllProjectsForDashboard(
+            @RequestParam String branchCode,
+            @RequestParam String role,
+            @RequestParam String email
+    ) {
+        List<ProjectDTO> projects = projectService.getAllProjectsForDashboard(role, email, branchCode);
 
+        List<Map<String, Object>> filteredResponse = projects.stream().map(p -> {
+            Map<String, Object> map = new LinkedHashMap<>();
+            map.put("projectName", p.getProjectName());
+            map.put("status", p.getStatus());
+            map.put("branchName", p.getBranchName());
+            map.put("departmentName", p.getDepartmentName());
+            map.put("startDate", p.getStartDate());
+            return map;
+        }).collect(Collectors.toList());
 
-
-
+        return ResponseEntity.ok(filteredResponse);
+    }
 
 }
